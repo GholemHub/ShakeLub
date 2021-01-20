bc0fa3deea3d4581731afb3d8d04265c82217d62
Golem <gnolazena@gmail.com>
2021-01-21 00:45:12 +0100

    Verify NOT WORKING

--- app/src/main/java/com/example/shakelab/HomeActivity.java
+++ app/src/main/java/com/example/shakelab/HomeActivity.java
@@ -3,6 +3,10 @@ package com.example.shakelab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shakelab/RegiterActivity.java
+++ app/src/main/java/com/example/shakelab/RegiterActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.ims.RegistrationManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -42,8 +43,9 @@ public class RegiterActivity extends AppCompatActivity {
 
     private TextView BackToSignIn;
 
-    private ProgressBar progressBar;
+    ProgressBar progressBar;
     private Button SignUp;
+    private Button btnVerify;
 
     public static final String TAG = "TAG";
 
@@ -56,6 +58,8 @@ public class RegiterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_window);
 
+
+
         BackToSignIn = findViewById(R.id.RegSignIn);
 
         textPassword = findViewById(R.id.EditTextPassword);
@@ -65,14 +69,10 @@ public class RegiterActivity extends AppCompatActivity {
         Policy = findViewById(R.id.checkBoxPolicy);
         SignUp = findViewById(R.id.BtnSignUp);
 
+
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
-
-        if(fAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-            finish();
-        }
 
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +80,13 @@ public class RegiterActivity extends AppCompatActivity {
                 String Email = textEmail.getText().toString().trim();
                 String Password = textPassword.getText().toString().trim();
                 String Nickname = textNickname.getText().toString().trim();
+
+
+                if(TextUtils.isEmpty(Nickname)){
+                    textEmail.setError("Nickname is Required");
+                    progressBar.setVisibility(View.INVISIBLE);
+                    return;
+                }
 
                 if(TextUtils.isEmpty(Email)){
                     textEmail.setError("Email is Required");
@@ -93,61 +100,50 @@ public class RegiterActivity extends AppCompatActivity {
                     return;
                 }
 
-                if(TextUtils.isEmpty(Nickname)){
-                    textEmail.setError("Nickname is Required");
+                if(!Policy.isActivated()){
+                    Policy.setError("Policy is Required");
                     progressBar.setVisibility(View.INVISIBLE);
                     return;
                 }
-
-                progressBar.setVisibility(View.VISIBLE);
 
                 fAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
 
-                            //send verification link
-
-                            FirebaseUser fuser = fAuth.getCurrentUser();
-                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(getApplicationContext(), "Verification Email Hes been Sent", Toast.LENGTH_SHORT).show();
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
-                                }
-                            });
-
-
-
-
-
                             Toast.makeText(RegiterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
                             userID = fAuth.getCurrentUser().getUid();
                             DocumentReference documentReference = fStore.collection("users").document(userID);
                             Map<String,Object> user = new HashMap<>();
                             user.put("nickname",Nickname);
                             user.put("email",Email);
+
                             documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
                                     Log.d(TAG, "onSuccess: user Profile is created for " + userID);
                                 }
                             });
+                                VerifyUser();
 
 
-                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                            try {
+                                if(fAuth.getCurrentUser().isEmailVerified()){
+                                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                                }
+
+                            }catch (Exception e){
+
+                            }
+
                         }else{
                             Toast.makeText(getApplicationContext(), "User Does not Created " +
                                     task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
                         }
                     }
                 });
-
-
             }
         });
 
@@ -158,5 +154,31 @@ public class RegiterActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void VerifyUser() {
+
+        //Toast.makeText(getApplicationContext(), "Verification Email Hes been Sent", Toast.LENGTH_SHORT).show();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        FirebaseUser fuser = fAuth.getCurrentUser();
+
+        fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "Verification Email Hes been Sent", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
+            }
+        });
     }
 }
