ed8a7d06f4289592cc8cc418950dc04708213a14
Golem <segeda.olexandr@gmail.com>
2021-01-25 20:21:42 +0100

    Google profile image

--- app/src/main/java/com/example/shakelab/HomeActivity.java
+++ app/src/main/java/com/example/shakelab/HomeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -18,15 +19,17 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 import javax.annotation.Nullable;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawer;
-    TextView users_email;
 
     FirebaseAuth fAuth;
 
@@ -49,15 +52,20 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         fAuth = FirebaseAuth.getInstance();
 
-        //users_email = findViewById(R.id.users_email);
-        //users_email.setText(fAuth.getCurrentUser().getEmail().toString());
-
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         View headerView = navigationView.getHeaderView(0);
         TextView navUsername = (TextView) headerView.findViewById(R.id.users_email);
         navUsername.setText(fAuth.getCurrentUser().getEmail());
+
+        if (fAuth != null){
+            //Display User Image from Google Account
+            //Objects.requireNonNull() prevents getPhotoUrl() from returning a NullPointerException
+            String personImage = Objects.requireNonNull(fAuth.getCurrentUser().getPhotoUrl()).toString();
+            ImageView navUserImage = (ImageView) headerView.findViewById(R.id.users_image);
+            Glide.with(this).load(personImage).into(navUserImage);
+        }
 
     }
 
